1783d7a86c3c9247f45215c93555c8adf6db41c8
civbruizer <gazencaio@gmail.com>
2022-04-06 20:15:59 -0300

    03

--- 03.cpp
+++ 03.cpp
@@ -1,10 +1,13 @@
 #include <stdio.h>
 #include <string.h>
 
+char p[50];
 int invert(int a){
-
+    printf("%c",);
 }
 
 int main(void){
-    
+    printf("digite uma palavra");
+    scanf("%s", p);
+
 };
