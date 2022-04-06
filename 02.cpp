b3a89dde4f4cd4cba272ee004b999720c02e6049
civbruizer <gazencaio@gmail.com>
2022-04-06 19:44:37 -0300

    1

--- 02.cpp
+++ 02.cpp
@@ -1,1 +1,20 @@
 #include <stdio.h>
+int i = 0;
+int n1=1, n2 = 0; 
+int fibonacci(void){
+    int nt;
+    i++;
+    if(i == 11){
+        return 0;
+    }
+    nt = n2;
+    n2 = n1 + n2;
+    n1 = nt;
+    printf("%d  ", n2);
+    fibonacci();
+}
+
+int main(void) {
+fibonacci();
+return 0;
+}
